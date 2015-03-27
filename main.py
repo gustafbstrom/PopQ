7dbf2e50d2f44340270642a76a45c9c1d60a5904
Tobias Ulvgard <tobias.ulvgard@gmail.com>
2015-03-27 10:32:32 +0100

    Fixed, file loading print irrespective of popresource and fixed multiword ansers

--- main.py
+++ main.py
@@ -12,18 +12,7 @@ import tempfile
 if __name__ == "__main__":
 
 
-    # ============================
-    # Traverse and load the input file list
-    # ========================
 
-    Questions = []
-
-    for filepath in sys.argv[1:]:
-        with open(filepath) as fp:
-            print "Opening: ",filepath
-            content = eval( fp.read() )
-            print "Loaded :",len(content)," questions"
-            Questions += content
 
 
     # touch a file for first use ,m
@@ -39,6 +28,23 @@ if __name__ == "__main__":
 
     lasttime = time.ctime(os.path.getmtime(fname))
     print "last modified: %s" % lasttime
+
+    # ============================
+    # Traverse and load the input file list
+    # ========================
+    Questions = []
+    for filepath in sys.argv[1:]:
+        with open(filepath) as fp:
+            print "Opening: ",filepath
+            content = eval( fp.read() )
+            print "Loaded :",len(content)," questions"
+            Questions += content
+
+    # ============================
+    # Ask 10 random questions 
+    # ========================
+
+
     correct = 10
 
     checklist = []
@@ -56,7 +62,7 @@ if __name__ == "__main__":
             print randq[0]
             ans = raw_input("> ")
             if ans != randq[1]:
-                    print "The answer is: %s" % randq[1]
+                    print "The answer is: %s" % ", ".join(randq[1:])
                     correct -= 1
         except KeyboardInterrupt:
             os.system("clear")
