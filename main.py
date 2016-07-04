0c67e860bb1c7c52c9acb5c4a9c02e388dc03892
Tobias Ulvgard <tobias.ulvgard@dirac.se>
2016-07-04 15:12:25 +0200

    Refactor read questions

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ class PopQ(object):
     resource_prefix = '/tmp/'
     temp_file = "popresource.txt"
 
+
     def _create_nonexisting_resourcefile(self, resource_file_path):
         if not os.path.isfile(resource_file_path):
            open(resource_file_path).close() 
+
 
     def _configure_popq(self, args):
         self.question_files = args.question_files
@@ -23,6 +25,7 @@ class PopQ(object):
             self.resource_file_path = args.resource_file
         else:
             self.resource_file_path = self.resource_prefix + self.temp_file
+
 
     def reset(self, num_correct):
         self._correct = num_correct
@@ -32,19 +35,9 @@ class PopQ(object):
         self.n_questions = 0
         self.total = 1
 
-    def __init__(self, num_correct=10):
 
-        self.reset(num_correct)
-
-        parser = argparse.ArgumentParser(description = 'PopQ - TUI pop quiz application')
-        parser.add_argument('-q', '--question_files', nargs='+', required = True, type = str)
-        parser.add_argument('-r', '--resource_file', type = str)
-        self._configure_popq(parser.parse_args())
-
-        self._create_nonexisting_resourcefile(self.resource_file_path)
-
-        print("Resource file: " + self.resource_file_path)
-
+    def read_questions(self, question_files):
+        questions = []
         for filepath in self.question_files:
             with open(filepath) as fp:
                 print("Opening: " + filepath)
@@ -52,13 +45,11 @@ class PopQ(object):
                 if fp_code:
                     content = eval(fp_code)
                     print("Loaded :",len(content)," questions")
-                    self.questions += content
-                    self.n_questions += len(content)
+                    questions += content
                 else:
                     print("Error while importing %s" % filepath)
         try:
             time.ctime(os.path.getmtime(self.fname))
-            #exit(0) # Why?
         except OSError as e:
             with file(self.fname, 'a'):
                 os.utime(self.fname, None)
@@ -66,6 +57,20 @@ class PopQ(object):
 
         lasttime = time.ctime(os.path.getmtime(self.fname))
         print("last modified: %s" % lasttime)
+        return questions 
+
+
+    def __init__(self, num_correct=10):
+        self.reset(num_correct)
+        parser = argparse.ArgumentParser(description = 'A terminal pop-question memory test')
+        parser.add_argument('-q', '--question_files', nargs='+', required = True, type = str)
+        parser.add_argument('-r', '--resource_file', type = str)
+        self._configure_popq(parser.parse_args())
+        print("Resource file: " + self.resource_file_path)
+        self._create_nonexisting_resourcefile(self.resource_file_path)
+        self.questions = self.read_questions(self.question_files)
+        self.n_questions = len(self.questions)
+
 
     def new_questionnaire(self):
         """Start a new questionnaire round"""
@@ -93,15 +98,18 @@ class PopQ(object):
                 os.system("clear")
                 exit(1)
     
+
     def __str__(self):
         """Presented score string from latest round"""
         return "Score: %1.2f%s [%d/%d]" % (float(self.correct)/self.total*100.,\
             '%', self.correct, self.total)
 
+
     def print_score(self):
         """Print the core from latest round"""
         os.system("clear")
         print(self.__str__())
+
 
 if __name__ == "__main__":
     Q = PopQ()
